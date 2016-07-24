b77e8e19bbe1196cbd9113c8b2b1a8f40f34896d
Glenn Vandeuren <vandeurenglenn@gmail.com>
2016-07-24 02:16:38 +0200

    :metal: Init & update submodule on install

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,9 @@
+echo 'Initializing & installing submodules'
+git submodule init
+git submodule update
 cd src/jouley
 echo 'Installing Bower components'
-bower i
+bower i --verbose
 echo 'Start build'
 polymer build
 cp build/bundled app/jouley
